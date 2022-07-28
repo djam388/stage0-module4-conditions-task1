7ca9c278741802b5158db650ad33f08f8b3c5067
djam388 <djam388@hotmail.com>
2022-07-28 16:31:27 +0500

    Attempt #1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean readyToPrint = true;
+        if (readyToPrint) {
+            System.out.println(true);
+        }
     }
 }
